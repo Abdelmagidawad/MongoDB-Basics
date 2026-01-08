4758a6510209d5c10f70719d9e00bf80ccc2582d
Abdelmagidawad <abdelmagidawad@gmail.com>
2026-01-09 00:08:01 +0200

    add in, nin, exists, projection conditions

--- MongoDB Commands/collection.find&operators filtering.js
+++ MongoDB Commands/collection.find&operators filtering.js
@@ -48,3 +48,25 @@
 =>db.books.find({$or:[{Pages:{$lt:100}},{Pages:{$gt:200}}]}) //to get all books with pages<100 and pages>200
 =>db.books.find({$or:[{Title:"Book4"},{$and:[{Pages:{$gt:0}},{Pages:{$lt:100}}]}]})
 //to get document book with title=Book4 or all books with pages in range 0 to 100
+
+// -In ,Nin ,Exists ,Projection-
+
+// $in(Comparison query operator) => to check the filed that is one of values insides list 
+// Syntax => {filed:{$in:[value1,value2,value3,...]}}
+=>db.books.find({Title:{$in:["Book1","Book2","Book3"]}}) //to return document book with title of Book1 or Book2 or Book3
+
+// $nin(not in)(Comparison query operator) => to check the filed that not equal values insides list 
+// Syntax => {filed:{$nin:[value1,value2,value3,...]}}
+=>db.books.find({Title:{$nin:["Book2","Book3"]}}) //to return all documents books with not title of Book2 or Book3
+
+// $exists(Element query operator) => matches document that have the specified field
+// Syntax =>{field:{$exists:<Boolean>}}
+=>db.books.find({Pages:{$exists:true}}) // to get all documents books with have field Pages
+=>db.books.find({Pages:{$exists:false}}) // to get all documents books with not have field Pages
+
+// Projection => to choice returns specified fields
+// projection is the object that have fields returns but filed=1 to return ,filed=0 to not return
+// Syntax =>db.collection.find({query},{projection})
+
+=>db.books.find({},{ISBN:1,Title:1}) // to get all documents books with Filed ISBN,title only
+=>db.books.find({},{Pages:0}) //  to get all documents books without filed Pages  
